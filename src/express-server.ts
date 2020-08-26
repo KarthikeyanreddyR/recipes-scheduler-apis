12bdd62876e42d0d8b4c64ce29e16aff27775c32
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2020-08-26 15:37:33 -0700

    renamed express config file

--- src/express-server.ts
+++ src/express-server.ts
@@ -1,13 +1,11 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 export class ExpressApplication {
     private app: express.Application;
-    private router: express.Router;
 
     constructor() {
         this.app = express();
-        this.router = express.Router();
-        this.manageEndpoints();
         this.middleware();
     }
 
@@ -15,13 +13,8 @@ export class ExpressApplication {
         return this.app;
     }
 
-    private manageEndpoints() {
-        this.router.get("/", (req: Request, res: Response) => {
-            res.send("Hello Karthikeyan!!");
-        });
-    }
-
     private middleware() {
-        this.app.use("/api", this.router);
+        this.app.disable('x-powered-by')
+        this.app.use(cors());
     }
 }
